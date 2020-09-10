064db96215c3274a49054c32d8fa63bc6f86bd6d
tianhao <416734061@qq.com>
2020-09-10 11:18:07 +0800

    fix missing MyDao imports

--- src/main/java/com/chris/config/MainConfig.java
+++ src/main/java/com/chris/config/MainConfig.java
@@ -3,10 +3,7 @@ package com.chris.config;
 import com.chris.bean.Person;
 import com.chris.dao.MyDao;
 import com.chris.filter.MyTypeFilter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 
@@ -16,10 +13,16 @@ import org.springframework.stereotype.Service;
 //        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {MyDao.class}),
         @ComponentScan.Filter(type = FilterType.CUSTOM, classes = {MyTypeFilter.class})
 }, useDefaultFilters = false)
+@Import(MyDao.class)
 public class MainConfig {
 
     @Bean(value = "person")
     public Person person(){
         return new Person("chris", 18);
+    }
+
+    @Bean(value = "person01")
+    public Person person01(){
+        return new Person("jack", 20);
     }
 }
